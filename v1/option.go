217fca35f56e28b51ad06137187fb8a4d9a26857
4thel00z <4thel00z@gmail.com>
2020-11-24 02:43:38 +0100

    [master] Add more methods to Option

--- v1/option.go
+++ v1/option.go
@@ -1,5 +1,9 @@
 package v1
 
+import (
+	"io"
+)
+
 type Option struct {
 	value interface{}
 	err   error
@@ -13,6 +17,31 @@ func (o Option) Or(i interface{}) interface{} {
 		return i
 	}
 	return nil
+}
+
+func (o Option) Read() Option {
+	if o.err != nil {
+		return Read(o.value.(io.Reader))
+	}
+	return o
+}
+func (o Option) Slurp() Option {
+	if o.err != nil {
+		return o
+	}
+	return Slurp(o.value.(io.ReadCloser))
+
+}
+
+func (o Option) ToString() Option {
+	if o.err != nil {
+		return o
+
+	}
+	return Option{
+		value: string(o.value.([]byte)),
+		err:   o.err,
+	}
 }
 
 func (o Option) Unwrap() interface{} {
@@ -29,10 +58,24 @@ func (o Option) UnwrapBytes() []byte {
 	return o.value.([]byte)
 }
 
+func (o Option) UnwrapString() string {
+	if o.err != nil {
+		panic(o.err)
+	}
+	return o.value.(string)
+}
+
 func (o Option) Catch(e ErrorHandler) interface{} {
 	if o.err != nil {
 		e(o.err)
 		return nil
 	}
 	return o.value
+}
+
+func (o Option) Close() Option {
+	if o.err == nil {
+		o.err = o.value.(io.Closer).Close()
+	}
+	return o
 }
