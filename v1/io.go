84b31fcdff869c7316bd7aa3130872aea1da08e8
4thel00z <4thel00z@gmail.com>
2020-11-24 02:43:07 +0100

    [master] Rename slurp file to io. Close io.ReadClosers in slurp and just read bytes in read

---
+++ v1/io.go
@@ -0,0 +1,37 @@
+package v1
+
+import (
+	"io"
+	"io/ioutil"
+	"os"
+)
+
+func Slurp(r io.ReadCloser) Option {
+	content, err := ioutil.ReadAll(r)
+	defer func() {
+		if err != nil {
+			r.Close()
+		}
+	}()
+
+	return Option{
+		value: content,
+		err:   err,
+	}
+}
+
+func Read(r io.Reader) Option {
+	content, err := ioutil.ReadAll(r)
+	return Option{
+		value: content,
+		err:   err,
+	}
+}
+
+func Open(path string) Option {
+	f, err := os.Open(path)
+	return Option{
+		value: f,
+		err:   err,
+	}
+}
