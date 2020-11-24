6ff1a46f1781aadbb6693faf9cb9efe3db926e63
4thel00z <4thel00z@gmail.com>
2020-11-24 03:06:43 +0100

    [master] Add an example where we read a json file into a struct in oneline

---
+++ example/json.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	λ "github.com/4thel00z/lambda/v1"
+	"strings"
+)
+
+type MagicSpell struct {
+	Name        string  `json:"name"`
+	AttackPower float64 `json:"attack_power"`
+	Type        string  `json:"type"`
+	Description string  `json:"description"`
+}
+
+func main() {
+	var (
+		m MagicSpell
+	)
+	λ.Open("magic.json").Slurp().JSON(&m).Catch(λ.Die)
+
+	fmt.Println(strings.Join([]string{m.Name, m.Type, fmt.Sprintf("%f", m.AttackPower), m.Description}, "\n"))
+}
