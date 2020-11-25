bcfde6010007ac84e9c4d3eae563da3bb98f2fb5
4thel00z <4thel00z@gmail.com>
2020-11-25 01:06:32 +0100

    [master] Add Error convenience util for creating errors and cry helper

--- v1/utils.go
+++ v1/utils.go
@@ -1,8 +1,16 @@
 package v1
 
-import "log"
+import (
+	"errors"
+	"log"
+)
 
 // Some convenience ErrorHandler
 
+func Cry(o Option) Option { log.Fatalln(o.Error()); return o }
 func Die(err error) error { log.Fatalln(err); return nil }
 func Ignore(error) error  { return nil }
+
+var (
+	Error = errors.New
+)
