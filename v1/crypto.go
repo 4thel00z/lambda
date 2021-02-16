fec1251a6b55dd779576ecf2d8d365e46e3b80c7
4thel00z <4thel00z@gmail.com>
2021-02-16 01:00:35 +0100

    [master] Add simple CBC mode AES encryption with PKCS7 padding
    Signed-off-by: 4thel00z <4thel00z@gmail.com>

---
+++ v1/crypto.go
@@ -0,0 +1,77 @@
+package v1
+
+import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"errors"
+	"io"
+)
+
+func AddPKCS7Padding(ciphertext []byte, blockSize int) []byte {
+	padding := blockSize - len(ciphertext)%blockSize
+	return append(ciphertext, bytes.Repeat([]byte{byte(padding)}, padding)...)
+}
+
+func RemovePKCS7Padding(origData []byte) []byte {
+	length := len(origData)
+	return origData[:(length - int(origData[length-1]))]
+}
+
+func (o Option) Encrypt(key []byte) Option {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return Option{
+			value: o.value,
+			err:   err,
+		}
+	}
+
+	blockSize := block.BlockSize()
+	src := AddPKCS7Padding(o.UnwrapBytes(), blockSize)
+	dst := make([]byte, blockSize+len(src))
+	iv := dst[:blockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return Option{
+			value: o.value,
+			err:   err,
+		}
+	}
+	mode := cipher.NewCBCEncrypter(block, iv)
+	mode.CryptBlocks(dst[blockSize:], src)
+
+	return Option{
+		value: dst,
+		err:   nil,
+	}
+}
+
+func (o Option) Decrypt(key []byte) Option {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return Wrap(o.value, err)
+	}
+	src := o.UnwrapBytes()
+	srcCopy := make([]byte, len(src))
+	copy(srcCopy,src)
+
+	blockSize := block.BlockSize()
+
+	if len(srcCopy) < blockSize {
+		return Wrap(o.value, errors.New("ciphertext too short"))
+	}
+	iv := srcCopy[:blockSize]
+	srcCopy = srcCopy[blockSize:]
+	if len(srcCopy)%blockSize != 0 {
+		return Wrap(o.value, errors.New("ciphertext is not a multiple of the block size"))
+	}
+	mode := cipher.NewCBCDecrypter(block, iv)
+
+	// CryptBlocks can work in-place if the two arguments are the same.
+	mode.CryptBlocks(srcCopy, srcCopy)
+	return Option{
+		value: RemovePKCS7Padding(srcCopy),
+		err:   nil,
+	}
+}
